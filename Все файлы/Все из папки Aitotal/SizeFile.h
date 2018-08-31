2e1a33681bb034b50f604e01a3e3eaa813dea3eb
DrDabin <Aibolit198@mail.ru>
2018-08-31 07:08:48 +0300

    Добавил исключение файлов

--- Все файлы/Все из папки Aitotal/SizeFile.h
+++
@@ -1,9 +0,0 @@
-//---------------------------------------------------------------------------
-
-#ifndef SizeFileH
-#define SizeFileH
-//---------------------------------------------------------------------------
-
-//__int64 MyFileSize(UnicodeString Path);
-__int64 MyFileSize2(UnicodeString Path);
-#endif
