b34513a87b1489591e2c585aaefdfdd2a26fb87c
DrDabin <Aibolit198@mail.ru>
2021-06-21 12:57:22 +0300

    fix bag version

--- AitotalInclude/AitotalVersion.h
+++ AitotalInclude/AitotalVersion.h
@@ -4,7 +4,7 @@
 #define MY_VER_MAJOR 3
 #define MY_VER_MINOR 4
 #define MY_VER_BUILD 3
-#define MY_VERSION "3.4.1\000"
+#define MY_VERSION "3.4.3\000"
 #define MY_DATE "2021.06.20\000"
 #define MY_AUTHOR_NAME "Alex\000"
 #define MY_COPYRIGHT "(c) Alex1983 "  MY_DATE
