a53a577d274985f697ad695f10842068570bbe38
DrDabin <Aibolit198@mail.ru>
2021-07-09 16:13:00 +0400

    fix.

---
+++ AitotalInclude/RusCaption.h
@@ -0,0 +1,11 @@
+#ifndef RusCaptionH
+#define RusCaptionH
+
+	//Заполняем название столбцов таблицы  LW1
+	UnicodeString LW1Name = "Имя файла";
+	UnicodeString LW1ColumnPatch = "Полное имя файла";
+	UnicodeString LW1ColumnFileSize = "";
+	UnicodeString LW1ColumnMD5File = "";
+	UnicodeString LW1ColumnSHAFile = "";
+
+#endif
